8b6f254d93c367f9e6401bd5427c1cece5c8870a
Steven Pav <steven@corecast.io>
2016-04-08 13:21:07 -0700

    references

--- man-roxygen/ref-bws.R
+++ man-roxygen/ref-bws.R
@@ -1,6 +1,6 @@
 #' @references
 #'
-#' W. Baumgartner, P. Weiss, H. Schindler, 'A Nonparametric Test for the General Two-Sample Problem', 
-#' Biometrics, Vol. 54, No. 3 (Sep., 1998), pp. 1129-1135. 
+#' W. Baumgartner, P. Weiss, H. Schindler, 'A nonparametric test for the general two-sample problem', 
+#' Biometrics 54, no. 3 (Sep., 1998): pp. 1129-1135. 
 #' \url{http://doai.io/10.2307/2533862}
 #' 
